126c4167267bf7e2d717dea6ae8c79e2a33f5cd8
aryankumar897 <82169659+aryankumar897@users.noreply.github.com>
2022-07-07 19:53:24 +0530

    firs

--- server.js
+++ server.js
@@ -1,31 +1,4 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@aryankumar897 
-aryankumar897
-/
-feedbackonline
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-Settings
-feedbackonline/server.js /
-@aryankumar897
-aryankumar897 make it better
-Latest commit 68cadf9 2 days ago
- History
- 1 contributor
-17 lines (13 sloc)  440 Bytes
+
    
 const express = require('express');
 const compression = require('compression');
@@ -44,16 +17,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`);
 });
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Loading complete
+
